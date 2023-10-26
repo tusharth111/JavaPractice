84ecee116eb6dd3546997cf3b45305415d656f16
Tushar Thakur <tusharth111@gmail.com>
2023-10-26 12:18:35 +0530

    added the question 25 from spread sheet add without operators

---
+++ src/BCA5Practical/AddWithoutOperators.java
@@ -0,0 +1,19 @@
+package BCA5Practical;
+
+public class AddWithoutOperators {
+    public static int add(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int sum = add(num1, num2);
+        System.out.println("Sum: " + sum);
+    }
+}
