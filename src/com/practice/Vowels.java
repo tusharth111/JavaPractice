1ca2faeaac317b92fe80c221130b81bc10f38076
Tushar Thakur <tusharth111@gmail.com>
2023-11-21 15:47:52 +0530

    added vowels checking program

---
+++ src/com/practice/Vowels.java
@@ -0,0 +1,30 @@
+package com.practice;
+
+public class Vowels {
+
+	int fun(String str) 
+	{
+		int i,c=0;
+		for(i =0;i<str.length();i++) 
+		{
+			char ch = str.charAt(i);
+			if(ch == 'a' || ch =='e'||ch =='i'|| ch == 'o'|| ch == 'u') 
+			{
+				c++;
+			}else if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') 
+			{
+				c++;
+			}
+		}
+		return c ;
+	}
+	public static void main(String[] args) 
+	{
+		String str = "HellO";
+		Vowels obj = new Vowels();
+		int ans = obj.fun(str);
+		System.out.println(ans);
+
+	}
+
+}
